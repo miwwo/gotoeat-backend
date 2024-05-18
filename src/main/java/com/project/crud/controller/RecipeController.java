792d0856e7f9093d29457f4762df6b63422a635e
Miwwo <saifer.angel@yandex.ru>
2024-05-18 12:27:03 +0300

    JWT Provider @Autowired fix

--- src/main/java/com/project/crud/controller/RecipeController.java
+++ src/main/java/com/project/crud/controller/RecipeController.java
@@ -5,6 +5,7 @@ import com.project.crud.service.RecipeService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
--- src/main/java/com/project/crud/security/JwtAuthFilter.java
+++ src/main/java/com/project/crud/security/JwtAuthFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,7 +16,9 @@ import java.io.IOException;
 
 public class JwtAuthFilter extends OncePerRequestFilter {
 
+    @Autowired
     private JwtProvider jwtProvider;
+    @Autowired
     private CustomUserDetailsService customUserDetailsService;
 
 
